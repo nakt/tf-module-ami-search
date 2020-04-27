84f5a590f2fa519a9ea5b6df0baa873a89b52e8f
Tetsuo Nakamura <tetsuo.nakamura@gmail.com>
2020-04-27 16:50:39 +0900

    Small update doc in variable.tf

--- variables.tf
+++ variables.tf
@@ -9,16 +9,16 @@ variable "os-maps" {
   type        = map(string)
 
   default = {
-    ubuntu       = "099720109477:^ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-.*"
-    ubuntu-1604  = "099720109477:^ubuntu/images/hvm-ssd/ubuntu-xenial-18.04-amd64-server-.*"
-    ubuntu-1804  = "099720109477:^ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-.*"
-    ubuntu-2004  = "099720109477:^ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-.*"
-    centos       = "679593333241:^CentOS.Linux.8.*x86_64.*"
-    centos-6     = "679593333241:^CentOS.Linux.6.*x86_64.*"
-    centos-7     = "679593333241:^CentOS.Linux.7.*x86_64.*"
-    centos-8     = "679593333241:^CentOS.Linux.8.*x86_64.*"
-    amazon       = "137112412989:^amzn2-ami-hvm-.*x86_64-gp2"
-    amazon-1     = "137112412989:^amzn-ami-hvm-.*x86_64-gp2"
-    amazon-2_lts = "137112412989:^amzn2-ami-hvm-.*x86_64-gp2"
+    ubuntu      = "099720109477:^ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-.*"
+    ubuntu-1604 = "099720109477:^ubuntu/images/hvm-ssd/ubuntu-xenial-18.04-amd64-server-.*"
+    ubuntu-1804 = "099720109477:^ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-.*"
+    ubuntu-2004 = "099720109477:^ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-.*"
+    centos      = "679593333241:^CentOS.Linux.8.*x86_64.*"
+    centos-6    = "679593333241:^CentOS.Linux.6.*x86_64.*"
+    centos-7    = "679593333241:^CentOS.Linux.7.*x86_64.*"
+    centos-8    = "679593333241:^CentOS.Linux.8.*x86_64.*"
+    amazon      = "137112412989:^amzn2-ami-hvm-.*x86_64-gp2"
+    amazon-1    = "137112412989:^amzn-ami-hvm-.*x86_64-gp2"
+    amazon-2    = "137112412989:^amzn2-ami-hvm-.*x86_64-gp2"
   }
 }
